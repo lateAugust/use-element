747e1c1bac4ee53f8c407e28309b7fc18eddebc2
dlh <qlhdtly12189@163.com>
2020-08-15 10:29:17 +0800

    引入全局方法和全局样式

--- src/main.js
+++ src/main.js
@@ -1,15 +1,19 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import ElementUI from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
+import "./App.scss";
 
-Vue.config.productionTip = false
+import gMethods from "./utils/methods";
+
+Vue.config.productionTip = false;
+Vue.prototype.gMethods = gMethods;
 
 Vue.use(ElementUI);
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
